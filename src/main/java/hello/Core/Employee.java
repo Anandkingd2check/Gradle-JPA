ce3bd1577c91bc9401ff4aaf5cdc7d31f25e2b5b
Anandkingd2check <anandurz@gmail.com>
2019-08-10 21:40:33 -0500

    Practise core java

---
+++ src/main/java/hello/Core/Employee.java
@@ -0,0 +1,15 @@
+package hello.Core;
+
+public class Employee {
+    float salary=4000;
+}
+
+class Programmer extends Employee{
+    int bonus=10000;
+
+    public static void main(String[] args) {
+        Programmer p = new Programmer();
+        System.out.println("Sal is" +p.salary);
+        System.out.println("Bonus is"+p.bonus);
+    }
+}
